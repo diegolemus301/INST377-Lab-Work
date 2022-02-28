5694f88b89441b42479b857a43e612b5c6b7ade8
diegolemus301 <dlemus11@terpmail.umd.edu>
2022-02-27 21:49:27 -0500

    Lab 5 Completion
    API request and form completed

---
+++ client/lab_5/script.js
@@ -0,0 +1,12 @@
+async function mainEvent() {
+  const form = document.querySelector('.box');
+  form.addEventListener('submit', async (submitEvent) => {
+    submitEvent.preventDefault();
+    console.log('form submission');
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const arrayFromJason = await results.json();
+    console.table(arrayFromJason);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
